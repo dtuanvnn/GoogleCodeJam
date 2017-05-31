be8481ffae7617fbfd4823d4a0092e34f3d532fa
Tuan Doan <tuan.doan@episerver.com>
2017-05-31 10:12:59 +0700

    Resolve pancake problem

---
+++ Qualification/OversizedPancake/Flipper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace OversizedPancake
+{
+    public class Flipper
+    {
+        public int NumberOfTestCase { get; set; }
+
+        public List<string> StateOfPancake { get; set; }
+
+        public List<int> FlipperSize { get; set; }
+        
+        public Flipper(int test, List<int> size, List<string> state)
+        {
+            NumberOfTestCase = test;
+            FlipperSize = size;
+            StateOfPancake = state;
+        }
+        
+        private bool HappyPancakes(string pancakes)
+        {
+            return !pancakes.Contains("-");
+        }
+
+        private string ChangeState(string pancakes, int startPos, int length)
+        {
+            var stringArr = pancakes.ToCharArray();
+            for (int i = startPos; i < startPos + length; i++)
+            {
+                var state = stringArr[i];
+                stringArr[i] = state == '-' ? '+' : '-';
+            }
+            return new string(stringArr);
+        }
+
+        private int DoFlip(int flipperSize, string pancakes)
+        {
+            var index = pancakes.IndexOf('-');
+            if (index < 0)
+            {
+                return 0;
+            }
+
+            var step = 0;
+            while (index > -1 && index <= pancakes.Length - flipperSize)
+            {
+                pancakes = ChangeState(pancakes, index, flipperSize);
+                index = pancakes.IndexOf('-');
+                step++;
+            }
+
+            if (HappyPancakes(pancakes))
+            {
+                return step;
+            }
+
+            return -1;
+        }
+
+        public List<int> DoWork()
+        {
+            var stepResults = new List<int>();
+            for (int i = 0; i < NumberOfTestCase; i++)
+            {
+                stepResults.Add(DoFlip(FlipperSize[i], StateOfPancake[i]));
+            }
+            return stepResults;
+        }
+    }
+}
---
+++ Qualification/PancakeFlipper/Program.cs
@@ -0,0 +1,65 @@
+﻿using OversizedPancake;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PancakeFlipper
+{
+    class Program
+    {
+        private static List<string> _states = new List<string>();
+        private static List<int> _flipperSizes = new List<int>();
+        private static int _testCases;
+
+        static void Main(string[] args)
+        {
+            ReadFile(@"A-small-practice.in");
+            var flipper = new Flipper(_testCases, _flipperSizes, _states);
+            var steps = flipper.DoWork();
+            WriteFile(@"A-small-practice.out", steps);
+
+            _testCases = 0;
+            _flipperSizes = new List<int>();
+            _states = new List<string>();
+
+            ReadFile(@"A-large-practice.in");
+            flipper.NumberOfTestCase = _testCases;
+            flipper.StateOfPancake = _states;
+            flipper.FlipperSize = _flipperSizes;
+            steps = flipper.DoWork();
+            WriteFile(@"A-large-practice.out", steps);
+        }
+
+        static void ReadFile(string filePath)
+        {
+            var file = new StreamReader(filePath);
+            _testCases = int.Parse(file.ReadLine());
+            for (int i = 0; i < _testCases; i++)
+            {
+                var line = file.ReadLine();
+                var subString = line.Split(' ');
+
+                _states.Add(subString[0]);
+                _flipperSizes.Add(int.Parse(subString[1]));
+            }
+
+            file.Close();
+        }
+
+        static void WriteFile(string filePath, List<int> steps)
+        {
+            using (StreamWriter file = new StreamWriter(filePath, true))
+            {
+                for (int i = 0; i < _testCases; i++)
+                {
+                    var result = steps[i].ToString();
+                    if (steps[i] < 0)
+                    {
+                        result = "IMPOSSIBLE";
+                    }
+
+                    file.WriteLine("Case #{0}: {1}", i + 1, result);
+                }
+            }
+        }
+    }
+}
